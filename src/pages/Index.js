0dd382f6c1050e5a01f1166018533b4ebe4abfe9
rukhshanarifm <rukhshan@uchicago.edu>
2025-03-14 03:43:28 +0500

    heading changes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,17 +14,17 @@ const Index = () => (
       <header>
         <div className="title">
           <h2>
-            <Link to="/">About this site</Link>
+            <Link to="/">Home</Link>
           </h2>
         </div>
       </header>
       <p>
         {' '}
-        Welcome to my website. Please feel free to read more{' '}
-        <Link to="/about">about me</Link>, or you can check out my{' '}
-        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
-        view <Link to="/stats">site statistics</Link>, or{' '}
-        <Link to="/contact">contact</Link> me.
+        Welcome to my personal website! Please feel free to read more{' '}
+        <strong><Link to="/about">about me</Link></strong>, or you can check out my{' '}
+        <strong><Link to="/resume">resume</Link></strong>, <strong><Link to="/projects">projects</Link></strong>,{' '}
+        view random<strong><Link to="/stats"> statistics</Link></strong>, or{' '}
+        <strong><Link to="/contact">contact</Link></strong> me.
         With experience as an independent contractor, I am open to globally remote opportunities,
         including short-term projects, long-term contracts, and consulting roles.
       </p>
